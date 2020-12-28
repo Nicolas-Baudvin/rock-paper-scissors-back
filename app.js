e7d8501938cc524f5882df471ef12d6f81890009
unknown <nicolas.baudvin@gmail.com>
2020-12-28 14:27:38 +0100

    set up server and socket

---
+++ app.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const helmet = require("helmet");
+const path = require("path");
+
+const app = express();
+
+// mongoose.connect(process.env.MONGO_CONNECTION_LINK,
+//     {
+//         "useNewUrlParser": true,
+//         "useUnifiedTopology": true
+//     })
+//     .then(() => console.log("Connexion à MongoDB réussie !"))
+//     .catch((e) => console.log("Connexion à MongoDB échouée !", e));
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000/");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+    next();
+});
+
+app.use(helmet());
+
+app.use(express.json());
+
+module.exports = app;
