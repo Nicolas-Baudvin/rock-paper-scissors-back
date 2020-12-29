84c88cf6739f5ba1fb5ada84a89836b76e8c2073
unknown <nicolas.baudvin@gmail.com>
2020-12-29 14:23:18 +0100

    emit error when room already exist

--- SocketController/createRoom.js
+++ SocketController/createRoom.js
@@ -1,19 +1,22 @@
-const createRoom = (socket, data, roomCreated, io) => {
+const createRoom = (socket, data, roomsCreated, io) => {
 
     const { roomName, username, id } = data;
 
     socket.join(roomName);
+    if (roomsCreated[roomName])
+    {
+        return socket.emit("fail creating room", "Room Already exist, try another name");
+    }
 
     const newRoom = {
         "name": roomName,
         "owner": { username, "uniqueID": id, "socketID": socket.id },
         "users": [{ "id": socket.id, username, "uniqueID": id }],
         "messages": [],
-        "scores": []
+        "scores": [0, 0]
     };
 
-    roomCreated[roomName] = newRoom;
-
+    roomsCreated[roomName] = newRoom;
     socket.emit("room created", newRoom);
 };
 
