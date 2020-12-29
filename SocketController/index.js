94607088105e7bd89ebe8a6568fc6dede557537f
unknown <nicolas.baudvin@gmail.com>
2020-12-29 14:23:40 +0100

    WIP: User disconnect logic

--- SocketController/index.js
+++ SocketController/index.js
@@ -1,7 +1,9 @@
 const createUser = require("./createUser");
 const createRoom = require("./createRoom");
+const logOut = require("./logOut");
+const joinRoom = require("./joinRoom");
 
-let roomCreated = {};
+let roomsCreated = {};
 
 const socketApp = (socket, io) => {
     console.log("Connexion en cours d'un utilisateur");
@@ -10,9 +12,31 @@ const socketApp = (socket, io) => {
 
     socket.on("disconnect", () => {
         console.log("Déconnexion d'un utilisateur");
+        const roomsKey = Object.keys(roomsCreated);
+
+        console.log(roomsKey);
+        if (roomsKey.length)
+        {
+            roomsKey.forEach((room) => {
+                if (roomsCreated[room].users[0].id === socket.id || roomsCreated[room].users[1].id === socket.id )
+                {
+                    console.log(`Supression de l'utilisateur de la salle ${room}`);
+                    const newUsersArray = roomsCreated[room].users.filter((user) => user.id !== socket.id);
+                    const lastUser = newUsersArray[0];
+                    
+                    roomsCreated[room].users = newUsersArray;
+                    roomsCreated[room].owner = lastUser;
+                    io.to(room).emit("user leave", roomsCreated[room]);
+                }
+            });
+        }
     });
 
-    socket.on("create room", (data) => createRoom(socket, data, roomCreated, io));
+    socket.on("create room", (data) => createRoom(socket, data, roomsCreated, io));
+
+    socket.on("join room", (data) => joinRoom(socket, data, roomsCreated, io));
+
+    socket.on("logout", () => logOut(socket));
 };
 
 module.exports = socketApp;
