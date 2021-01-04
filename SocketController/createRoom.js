f81348826dd799693c70bae79bb6c83a4acbf1c9
unknown <nicolas.baudvin@gmail.com>
2021-01-04 14:00:30 +0100

    add shots and scores properties to rooms created

--- SocketController/createRoom.js
+++ SocketController/createRoom.js
@@ -2,18 +2,19 @@ const createRoom = (socket, data, roomsCreated, io) => {
 
     const { roomName, username, id } = data;
 
-    socket.join(roomName);
     if (roomsCreated[roomName])
     {
         return socket.emit("fail creating room", "Room Already exist, try another name");
     }
+    socket.join(roomName);
 
     const newRoom = {
         "name": roomName,
         "owner": { username, "uniqueID": id, "socketID": socket.id },
         "users": [{ "id": socket.id, username, "uniqueID": id }],
         "messages": [],
-        "scores": [0, 0]
+        "scores": { [username]: 0 },
+        "shots": []
     };
 
     roomsCreated[roomName] = newRoom;
