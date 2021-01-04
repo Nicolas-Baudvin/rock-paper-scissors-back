42bf931d80e86fe56d694ffedc92c1e70d08476d
unknown <nicolas.baudvin@gmail.com>
2021-01-04 14:04:00 +0100

    Clear a room when no user inside

--- SocketController/logOut.js
+++ SocketController/logOut.js
@@ -1,4 +1,14 @@
-const logOut = (socket) => {
+const clearUnusedRoom = (roomName, roomsCreated) => {
+    const users = roomsCreated[roomName].users.length;
+
+    if (!users.length)
+    {
+        delete roomsCreated[roomName];
+    }
+};
+
+const logOut = (socket, roomName, roomsCreated) => {
+    clearUnusedRoom(roomName, roomsCreated);
     socket.disconnect();
 };
 
