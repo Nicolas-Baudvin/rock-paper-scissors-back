c26329a62d327b96476643e5026d38740fb0a226
unknown <nicolas.baudvin@gmail.com>
2021-01-04 14:03:26 +0100

    add a score property with the username of new user as key

--- SocketController/joinRoom.js
+++ SocketController/joinRoom.js
@@ -1,13 +1,21 @@
 const joinRoom = (socket, data, roomsCreated, io) => {
     const { roomName, username, id } = data;
+    const scores = roomsCreated[roomName].scores;
 
     if (!roomsCreated[roomName])
     {
         return socket.emit("fail join", "No room with this name !");
     }
 
+    if (roomsCreated[roomName].users.length >= 2)
+    {
+        return socket.emit("fail join", "This room is full");
+    }
+
     socket.join(roomName);
     roomsCreated[roomName].users.push({ "id": socket.id, username, "uniqueID": id });
+    roomsCreated[roomName].scores = { ...scores, [username]: 0 };
+
     socket.emit("room joined", roomsCreated[roomName]);
     io.to(roomName).emit("user join", roomsCreated[roomName]);
 };
