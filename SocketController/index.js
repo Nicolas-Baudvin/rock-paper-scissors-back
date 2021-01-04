8004636fb522c5f98212aadf81be1330f07e1b31
unknown <nicolas.baudvin@gmail.com>
2021-01-04 14:01:30 +0100

    Add some Socket.io events : 'Play again', 'send shot type'

--- SocketController/index.js
+++ SocketController/index.js
@@ -2,6 +2,9 @@ const createUser = require("./createUser");
 const createRoom = require("./createRoom");
 const logOut = require("./logOut");
 const joinRoom = require("./joinRoom");
+const sendShotType = require("./sendShotType");
+const disconnect = require("./disconnect");
+const playAgain = require("./playAgain");
 
 let roomsCreated = {};
 
@@ -9,34 +12,18 @@ const socketApp = (socket, io) => {
     console.log("Connexion en cours d'un utilisateur");
 
     socket.on("new user", (username) => createUser({ socket, username }));
+    
+    socket.on("play again", (roomName) => playAgain(roomName, roomsCreated, socket, io));
 
-    socket.on("disconnect", () => {
-        console.log("Déconnexion d'un utilisateur");
-        const roomsKey = Object.keys(roomsCreated);
+    socket.on("send shot type", (data) => sendShotType(data, socket, io, roomsCreated));
 
-        console.log(roomsKey);
-        if (roomsKey.length)
-        {
-            roomsKey.forEach((room) => {
-                if (roomsCreated[room].users[0].id === socket.id || roomsCreated[room].users[1].id === socket.id )
-                {
-                    console.log(`Supression de l'utilisateur de la salle ${room}`);
-                    const newUsersArray = roomsCreated[room].users.filter((user) => user.id !== socket.id);
-                    const lastUser = newUsersArray[0];
-                    
-                    roomsCreated[room].users = newUsersArray;
-                    roomsCreated[room].owner = lastUser;
-                    io.to(room).emit("user leave", roomsCreated[room]);
-                }
-            });
-        }
-    });
+    socket.on("disconnect", () => disconnect(roomsCreated, socket, io));
 
     socket.on("create room", (data) => createRoom(socket, data, roomsCreated, io));
 
     socket.on("join room", (data) => joinRoom(socket, data, roomsCreated, io));
 
-    socket.on("logout", () => logOut(socket));
+    socket.on("logout", (roomName) => logOut(socket, roomName, roomsCreated));
 };
 
 module.exports = socketApp;
