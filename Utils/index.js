73c420b0da56ed2fbb3012a07afba67cf8e668be
unknown <nicolas.baudvin@gmail.com>
2021-01-04 14:05:03 +0100

    Utils win Rules

---
+++ Utils/index.js
@@ -0,0 +1,5 @@
+const shotTypeObject = require("./shotTypeObject");
+const winRulesObject = require("./winRulesObject");
+
+exports.shotTypeObject = shotTypeObject;
+exports.winRulesObject = winRulesObject;
