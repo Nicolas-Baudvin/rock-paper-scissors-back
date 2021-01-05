6929325d061d1781c00b7ae4f32bc5ed271e9e84
unknown <nicolas.baudvin@gmail.com>
2021-01-05 11:54:20 +0100

    delete unused rooms after the last user leave it

--- SocketController/disconnect.js
+++ SocketController/disconnect.js
@@ -3,18 +3,25 @@ const disconnect = (roomsCreated, socket, io) => {
     console.log("Déconnexion d'un utilisateur");
     const roomsKey = Object.keys(roomsCreated);
 
-    if (roomsKey.length)
-    {
+    if (roomsKey.length) {
         roomsKey.forEach((room) => {
-            if (roomsCreated[room].users[0].id === socket.id || roomsCreated[room].users[1].id === socket.id)
-            {
+            console.log("Salle : ", roomsCreated[room]);
+            if (roomsCreated[room].users[0].id === socket.id || roomsCreated[room].users[1].id === socket.id) {
                 console.log(`Supression de l'utilisateur de la salle ${room}`);
                 const newUsersArray = roomsCreated[room].users.filter((user) => user.id !== socket.id);
+
+                if (!newUsersArray.length) {
+                    delete roomsCreated[room];
+                    console.log("Suppression de la salle", room);
+                    return;
+                }
+                
                 const lastUser = newUsersArray[0];
 
                 roomsCreated[room].users = newUsersArray;
                 roomsCreated[room].owner = lastUser;
-                io.to(room).emit("user leave", roomsCreated[room]);
+                roomsCreated[room].scores = { [lastUser.username]: 0 };
+                return io.to(room).emit("user leave", roomsCreated[room]);
             }
         });
     }
