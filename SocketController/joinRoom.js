2133e7059e29655ab80aaeab3eb3332a726833fe
unknown <nicolas.baudvin@gmail.com>
2021-01-11 11:50:30 +0100

    break main func

--- SocketController/joinRoom.js
+++ SocketController/joinRoom.js
@@ -1,23 +1,31 @@
+const updateRoom = ({ roomsCreated, roomName, socket, username }) => {
+    const scores = roomsCreated[roomName].scores;
+    const roomUpdated = roomsCreated[roomName];
+
+    roomUpdated.users.push({ "id": socket.id, username });
+    roomUpdated.scores = { ...scores, [username]: 0 };
+    return roomUpdated;
+};
+
 const joinRoom = (socket, data, roomsCreated, io) => {
     const { roomName, username } = data;
-    
+
     if (!roomsCreated[roomName])
     {
         return socket.emit("fail join", "No room with this name !");
     }
-    
+
     if (roomsCreated[roomName].users.length >= 2)
     {
         return socket.emit("fail join", "This room is full");
     }
-    const scores = roomsCreated[roomName].scores;
-    
-    socket.join(roomName);
-    roomsCreated[roomName].users.push({ "id": socket.id, username });
-    roomsCreated[roomName].scores = { ...scores, [username]: 0 };
 
-    socket.emit("room joined", roomsCreated[roomName]);
-    io.to(roomName).emit("user join", roomsCreated[roomName]);
+    socket.join(roomName);
+
+    const roomUpdated = updateRoom({ roomsCreated, roomName, socket, username });
+
+    socket.emit("room joined", roomUpdated);
+    io.to(roomName).emit("user join", roomUpdated);
 };
 
 module.exports = joinRoom;
