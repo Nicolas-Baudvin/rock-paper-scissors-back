581bde29da810e6ad59c628eeb4d9b45f756681f
unknown <nicolas.baudvin@gmail.com>
2021-01-11 11:50:48 +0100

    fix syntax

--- SocketController/playAgain.js
+++ SocketController/playAgain.js
@@ -2,9 +2,8 @@ const playAgain = (roomName, roomsCreated, socket, io) => {
     const scores = roomsCreated[roomName].shots;
 
     if (scores.length !== 1)
-    {
         roomsCreated[roomName].shots = [];
-    }
+
     socket.emit("shots reinitialized", roomsCreated[roomName]);
 };
 
