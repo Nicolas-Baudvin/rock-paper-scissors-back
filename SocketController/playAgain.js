c88079619985212af23944b67d1b3786a97471c0
unknown <nicolas.baudvin@gmail.com>
2021-01-15 12:27:12 +0100

    clear winner from room

--- SocketController/playAgain.js
+++ SocketController/playAgain.js
@@ -2,7 +2,10 @@ const playAgain = (roomName, roomsCreated, socket, io) => {
     const scores = roomsCreated[roomName].shots;
 
     if (scores.length !== 1)
+    {
         roomsCreated[roomName].shots = [];
+        roomsCreated[roomName].winner = null;
+    }
 
     socket.emit("shots reinitialized", roomsCreated[roomName]);
 };
--- SocketController/sendShotType.js
+++ SocketController/sendShotType.js
@@ -1,26 +1,45 @@
 const { winRulesObject } = require("../Utils");
 
+const getGameWinner = (playerOne, playerTwo) => {
+    const isWin = winRulesObject[playerOne.shotType][playerTwo.shotType];
+    const winner = isWin ? isWin !== "equal" ? playerOne.username : "equal" : playerTwo.username;
+
+    return winner;
+};
+
+const incrementScoreForWinner = (scores, winner) => ({
+    ...scores,
+    [winner]: scores[winner] + 1
+});
+
+const updateRoom = (newRoom, roomsCreated, roomName) => {
+    roomsCreated[roomName] = newRoom;
+};
+
 const sendShotType = (data, socket, io, roomsCreated) => {
     const { shotType, username, roomName } = data;
-    const shots = roomsCreated[roomName].shots;
+    const room = roomsCreated[roomName];
+    const shots = room.shots;
 
-    roomsCreated[roomName].shots = [...shots, { username, shotType }];
+    room.shots = [...shots, { username, shotType }];
 
-    const playerOne = roomsCreated[roomName].shots[0];
-    const playerTwo = roomsCreated[roomName].shots[1];
+    const playerOne = room.shots[0];
+    const playerTwo = room.shots[1];
 
-    if (roomsCreated[roomName].shots.length === 2)
+    if (room.shots.length === 2)
     {
-        const isWin = winRulesObject[playerOne.shotType][playerTwo.shotType];
-        const winner = isWin ? isWin !== "equal" ? playerOne.username : "equal" : playerTwo.username;
-        const scores = roomsCreated[roomName].scores;
+        const winner = getGameWinner(playerOne, playerTwo);
+        const scores = room.scores;
 
         if (winner !== "equal")
         {
-            roomsCreated[roomName].scores = { ...scores, [winner]: scores[winner] + 1 };
+            room.scores = incrementScoreForWinner(scores, winner);
         }
-        roomsCreated[roomName].winner = winner;
-        io.to(roomName).emit("game result", roomsCreated[roomName]);
+        room.winner = winner;
+
+        updateRoom(room, roomsCreated, roomName);
+
+        io.to(roomName).emit("game result", room);
     }
 };
 
